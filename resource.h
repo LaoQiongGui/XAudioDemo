de39e1ff381330d76b5adf021ff109c67d70db34
LaoQiongGui <2307712195@qq.com>
2023-04-25 07:23:23 +0800

    修改：升级消息处理函数

--- resource.h
+++ resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ 生成的包含文件。
 // 供 XAudioDemo.rc 使用
 //
+#define IDOK2                           2
 #define IDM_ABOUTBOX                    0x0010
 #define IDS_ABOUTBOX                    101
 #define IDD_XAUDIODEMO_DIALOG           102
@@ -11,7 +12,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101

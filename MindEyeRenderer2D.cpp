b61c7414509e5bf99a313dc6b012ec29e8042208
LaoQiongGui <2307712195@qq.com>
2023-07-16 20:25:17 +0800

    修改：音效渲染器基类

---
+++ MindEyeRenderer2D.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "MindEyeRenderer2D.h"
+
+MindEyeRenderer2D::MindEyeRenderer2D()
+{
+}
+
+MindEyeRenderer2D::~MindEyeRenderer2D()
+{
+}
+
+void MindEyeRenderer2D::DoProcess(const MindEyeRenderer2DParams&, FLOAT32* __restrict pData, UINT32 cFrames, UINT32 cChannels)
+{
+}
--- MindEyeRenderer2D.h
+++ MindEyeRenderer2D.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "Common.h"
+#include "MindEyeRendererBase.h"
+
+struct MindEyeRenderer2DParams
+{
+	SpaceInfo spaceInfoEmitter;
+	SpaceInfo spaceInfoListener;
+};
+
+class __declspec(uuid("{5EB8D611-FF96-429d-8365-2DDF89A7C1CD}"))
+	MindEyeRenderer2D : public MindEyeRendererBase<MindEyeRenderer2D, MindEyeRenderer2DParams>
+{
+public:
+	MindEyeRenderer2D();
+	~MindEyeRenderer2D();
+
+	void DoProcess(const MindEyeRenderer2DParams &, _Inout_updates_all_(cFrames *cChannels) FLOAT32 *__restrict pData, UINT32 cFrames, UINT32 cChannels) override;
+};

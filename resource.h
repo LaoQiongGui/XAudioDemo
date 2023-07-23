5940ec4e7f5bba7c7deba20172a52e8f265bc363
LaoQiongGui <2307712195@qq.com>
2023-07-23 21:25:58 +0800

    新增：音频文件转存（读取）

--- resource.h
+++ resource.h
@@ -11,6 +11,7 @@
 #define IDC_EDIT2                       1003
 #define DISPLAY                         1003
 #define FFT_BUTTON                      1004
+#define SAVE_BUTTON                     1005
 
 // Next default values for new objects
 // 
@@ -18,7 +19,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
